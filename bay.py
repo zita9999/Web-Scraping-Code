eb3f2714e911aa8046a2f3c5422bc548e8e35470
Christopher Zita <53668760+zita9999@users.noreply.github.com>
2020-04-15 10:16:21 -0400

    Update bay.py

--- bay.py
+++ bay.py
@@ -2,11 +2,13 @@
 import scrapy
 
 n = 3
+#identifying the website that is gonna be scraped
 class BaySpider(scrapy.Spider):
     name = 'bay'
     allowed_domains = ['www.thebay.com']
     start_urls = ['https://www.thebay.com/search/EndecaSearch.jsp?N=1553+302023689']
 
+    #loop through every item grabbing the data we want
     def parse(self, response):
         global n
         i = 3
@@ -39,6 +41,7 @@ class BaySpider(scrapy.Spider):
         print (n)
         next_page = response.xpath("((//ol)[2]/li)["+nn+"]/a/@href").get()
         print(next_page)
+        #go to the next page when done with the current one
         if next_page:
             yield scrapy.Request(url = 'https://thebay.com'+next_page, callback = self.parse, dont_filter = True)
             
